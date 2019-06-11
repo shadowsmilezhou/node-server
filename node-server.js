e67ec981d9b28ccb855adc3ec2893381c7e90c22
zhouzhou <630551760@qq.com>
2019-06-11 17:46:26 +0800

    commit

---
+++ node-server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const multer  = require('multer');
+const cors = require('cors');
+const upload = multer({ dest: 'imgs/' });
+const p = require('path');
+
+const app = express();
+
+
+app.post('/upload', cors(), upload.single('file'), function (req, res, next) {
+    console.log('come in');
+    res.set("Access-Control-Allow-Origin" ,'*');
+    res.json({id: req.file.filename})
+});
+
+
+app.get('/preview/:key', cors(), function(req, res, next){
+    res.sendFile(`imgs/${req.params.key}`, {
+        root: __dirname,
+        headers:{
+            'Content-Type': 'image/jpeg',
+
+        },
+    }, (error)=>{
+        if(error){
+            res.status(404).send('Not found')
+        }
+    })
+});
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!')
+});
